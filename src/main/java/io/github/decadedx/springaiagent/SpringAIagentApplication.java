21f0ec20092c0f9d3146cce9d8e5ce0fe866655c
DX <1723069200@qq.com>
2026-09-19 15:51:34 +0800

    fix(docker): 将docker中的mysql端口进行修改

--- src/main/java/io/github/decadedx/springaiagent/SpringAIagentApplication.java
+++ src/main/java/io/github/decadedx/springaiagent/SpringAIagentApplication.java
@@ -3,10 +3,18 @@ package io.github.decadedx.springaiagent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 @SpringBootApplication
 public class SpringAIagentApplication {
 
     public static void main(String[] args) {
+//        SecureRandom random = new SecureRandom();
+//        byte[] key = new byte[32];
+//        random.nextBytes(key);
+//        String secret = Base64.getEncoder().encodeToString(key);
+//        System.out.println(secret);
         SpringApplication.run(SpringAIagentApplication.class, args);
     }
 
